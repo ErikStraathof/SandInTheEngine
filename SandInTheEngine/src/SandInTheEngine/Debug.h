ce2a379f400c5728bb8ecf327b7de3886ff25902
Erik <erik742000@gmail.com>
2025-05-31 18:30:45 +0200

    more logging beginnings

---
+++ SandInTheEngine/src/SandInTheEngine/Debug.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace SandInTheEngine
+{
+	
+	class SAND_IN_THE_ENGINE_API Debug
+	{
+	public: 
+		Debug();
+		~Debug();
+
+
+	};
+}
